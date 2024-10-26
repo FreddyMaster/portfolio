0ebe27f4ff2faf15663793911f80f892b5dea390
FreddyMaster <blackcatshd@gmail.com>
2024-10-25 21:03:25 -0400

    WIP styling on the main page and made resuable components

---
+++ src/lib/ProjectCard.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let title;
+    export let imageSrc;
+    export let imageAlt;
+    export let description;
+    export let link;
+  </script>
+  
+  <div class="card shadow-lg rounded-lg overflow-hidden max-w-xs mx-auto h-full flex flex-col">
+    <header class="card-header p-4">
+      <h2 class="text-xl font-bold text-center">{title}</h2>
+    </header>
+    <section class="p-4 flex-grow flex flex-col">
+      <div class="flex justify-center">
+        <img src={imageSrc} alt={imageAlt} />
+      </div>
+      <p class="my-4 text-gray-300">{description}</p>
+      <div class="mt-auto">
+        <a href={link} class="btn variant-filled-secondary w-full">Learn More</a>
+      </div>
+    </section>
+  </div>
+  
---
+++ src/lib/SkillCard.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { ProgressBar } from "@skeletonlabs/skeleton";
+    import Icon from '@iconify/svelte';
+    export let skillName;
+    export let description;
+    export let icon;
+    export let progressValue;
+  </script>
+  
+  <article class="card shadow-lg rounded-lg overflow-hidden max-w-xs mx-auto h-full flex flex-col">
+    <header class="card-header p-4">
+      <h2 class="text-xl font-bold text-center">{skillName}</h2>
+    </header>
+    <section class="p-4 flex-grow flex flex-col">
+      <div class="flex justify-center">
+        <Icon icon="{icon}" width="5rem" height="5rem"  style="color: white"/>
+      </div>
+      <p class="my-4 text-gray-300">{description}</p>
+      <div class="mt-auto py-4">
+      <ProgressBar value={progressValue} max={100} />
+      </div>
+    </section>
+  </article>
+  
